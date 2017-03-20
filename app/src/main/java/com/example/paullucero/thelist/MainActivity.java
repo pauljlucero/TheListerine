21709d731b460da07c76059587a9fb1be4e1fa57
paullucero <pauljlucero@gmail.com>
2017-03-20 14:22:29 -0600

    this is the user upload version. it doesnt connect to a database yet. it only accesses photos

--- app/src/main/java/com/example/paullucero/thelist/MainActivity.java
+++ app/src/main/java/com/example/paullucero/thelist/MainActivity.java
@@ -1,12 +1,42 @@
 package com.example.paullucero.thelist;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private static final int RESULT_LOAD_IMAGE = 1;
+    ImageView imageToUpload;
+    Button bUploadImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        imageToUpload = (ImageView) findViewById(R.id.imageToUpload);
+        bUploadImage = (Button) findViewById(R.id.bUploadImage);
+        imageToUpload.setOnClickListener(this);
+        bUploadImage.setOnClickListener(this);
     }
-}
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RESULT_LOAD_IMAGE && requestCode == RESULT_OK && data != null) {
+            Uri selectedImage = data.getData();
+            imageToUpload.setImageURI(selectedImage);
+        }}
+    @Override
+    public void onClick(View v){
+        switch (v.getId()){
+            case R.id.imageToUpload:
+                Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(galleryIntent, RESULT_LOAD_IMAGE);
+                break;
+            case R.id.bUploadImage:
+                break;
+        }}}
